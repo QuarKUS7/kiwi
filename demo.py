82215e199a760462fd5886a0afffb40822330d0a
Peter Pagac <quarkus7@gmail.com>
2019-03-25 16:48:50 +0100

    removed old files

--- demo.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-url = "https://api.skypicker.com/locations?type=subentity&term=GB&locale=en-US&active_only=false&location_types=airport&limit=999&sort=name"
-
-response = requests.get(url)
-data = response.json()
-
-print(data)
